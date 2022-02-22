e9d53cf7a57875948bceda71d837ea9c405a65c4
Shawn-Fries <samfoley275@gmail.com>
2022-02-22 16:17:22 -0500

    working on auth

--- BenchBnB/app/controllers/api/sessions_controller.rb
+++ BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      sign_in!(@user)
+      redirect_to api_users_url
+    else
+      render json: ['Invalid credentials'], status: 401
+    end
+  end
+
+    def destroy
+        if current_user
+            sign_out!
+            render {}
+        else
+            render json: ['No current user'], status: 404
+        end
+    end
 end
--- BenchBnB/app/controllers/api/users_controller.rb
+++ BenchBnB/app/controllers/api/users_controller.rb
@@ -1,2 +1,16 @@
 class Api::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            sign_in!(@user)
+            redirect_to api_users_url
+        else
+            render json: ['Username already exists'], status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:password, :username)
+    end
 end
